65056508fe0d34ead8d6deed71b4c9daf8d6b33a
Joshua M. Dotson <josdotso@cisco.com>
2017-11-16 17:51:23 -0500

    Add a "print license" subcommand

---
+++ cmd/license.go
@@ -0,0 +1,47 @@
+// Copyright © 2017 Cisco Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// licenseCmd represents the license command
+var licenseCmd = &cobra.Command{
+	Use:   "license",
+	Short: "Print license information.",
+	Long:  `Print license information.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(`// Copyright © 2017 Cisco Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.`)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(licenseCmd)
+}
